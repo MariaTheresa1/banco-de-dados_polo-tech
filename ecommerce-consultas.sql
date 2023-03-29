26258688c932904bfbca4e2fbbc5f85a3800da8c
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2023-03-29 20:53:29 -0300

    questão 4 e 5

--- ecommerce-consultas.sql
+++ ecommerce-consultas.sql
@@ -7,3 +7,8 @@ select * from pedido p where p.data_criacao >= '2023-01-01' and p.data_criacao <
 -- 3
 select * from pedido p where extract(month from p.data_criacao) = 01;
 
+-- 4
+select * from pedido p, item_pedido i where p.id = i.id_pedido and i.valor >= 2 and i.valor <= 5;
+
+-- 5
+select MAX(item_pedido.valor) from item_pedido;

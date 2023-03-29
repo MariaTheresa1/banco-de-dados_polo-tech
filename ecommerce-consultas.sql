2b9cd98936c42cf238449659b5870a68418e1e61
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2023-03-29 10:09:20 -0300

    questão 3

--- ecommerce-consultas.sql
+++ ecommerce-consultas.sql
@@ -4,3 +4,6 @@ select * from cliente c where c.nome like 'Ana %';
 -- 2
 select * from pedido p where p.data_criacao >= '2023-01-01' and p.data_criacao < '01-01-2024';
 
+-- 3
+select * from pedido p where extract(month from p.data_criacao) = 01;
+

fad2b0b589c1df926e5e1cd4c227a94a7fa92633
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2023-03-29 21:17:48 -0300

    correção na questão 2, 3 e 5 e questão 6

--- ecommerce-consultas.sql
+++ ecommerce-consultas.sql
@@ -2,13 +2,16 @@
 select * from cliente c where c.nome like 'Ana %';
 
 -- 2
-select * from pedido p where p.data_criacao >= '2023-01-01' and p.data_criacao < '01-01-2024';
+select * from pedido p where p.data_criacao >= '2023-01-01' and p.data_criacao < '2024-01-01';
 
 -- 3
 select * from pedido p where extract(month from p.data_criacao) = 01;
 
 -- 4
-select * from pedido p, item_pedido i where p.id = i.id_pedido and i.valor >= 2 and i.valor <= 5;
+select * from item_pedido i where i.valor >= 2 and i.valor <= 5;
 
 -- 5
-select MAX(item_pedido.valor) from item_pedido;
+select MAX(item_pedido.valor) as item_mais_caro from item_pedido;
+
+-- 6
+select distinct pedido.status from pedido;

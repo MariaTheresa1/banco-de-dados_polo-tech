f74efa3d303f0d46b77c94554a5fd2b87b3ef167
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2023-03-31 20:30:12 -0300

    questão 20, 21, 22, 23 e 24

--- ecommerce-consultas.sql
+++ ecommerce-consultas.sql
@@ -50,3 +50,39 @@ inner join item_carrinho ic on ic.id_cliente = c.id
 inner join produto p on p.id = ic.id_produto
 group by c.id 
 order by count(p);
+
+-- 20
+select ((sum(ip.quantidade * ip.valor)-sum(cp.valor))/count(ped.id)) as ticket_medio from pedido ped
+	inner join item_pedido ip  on ped.id = ip.id_pedido
+	left join cupom cp on cp.id = ped.id_cupom 
+	where ped.status='SUCESSO';
+
+-- 21
+select cl.id id_cliente, cl.nome, max(valor_pedido) valor_maximo_gasto
+	from (select ip.id_pedido, sum(ip.quantidade * ip.valor) as valor_pedido from item_pedido ip
+			group by ip.id_pedido) as valor_pedido
+		inner join pedido ped on ped.id = valor_pedido.id_pedido
+		inner join cliente cl on ped.id_cliente = cl.id
+		where valor_pedido > 10000
+		group by cl.id;
+		
+-- 22
+select c.descricao, p.id_cupom, count(p.id_cupom) quantitade_utilizada, sum(c.valor) valor_total_descontado from cupom c 
+	inner join pedido p on p.id_cupom = c.id
+	group by p.id_cupom, c.descricao
+	order by quantitade_utilizada desc limit 10;
+
+-- 23
+select c.descricao, p.id_cupom, count(p.id_cupom) quantitade_utilizada, c.limite_maximo_usos from cupom c
+	inner join pedido p on p.id_cupom = c.id
+	group by p.id_cupom, c.descricao, c.limite_maximo_usos
+	having count(p.id_cupom) > c.limite_maximo_usos 
+	order by quantitade_utilizada;
+
+-- 24
+select ped.id id_pedido, p.codigo_barras, c.id id_cliente, e.uf uf_cliente from item_pedido ip
+	inner join produto p on ip.id_produto = p.id
+	inner join pedido ped on ped.id = id_pedido
+	inner join cliente c on ped.id_cliente = c.id 
+	inner join endereco e on c.id_endereco = e.id 
+	where p.codigo_barras = '97692630963921' and e.uf = 'SP';
